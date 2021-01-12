187af2ebffc64e35ce9cf8051509d1faafc00eb9
Myroslav Pasko <myroslav.pasko@arvosoftware.com>
2021-01-12 09:06:07 +0200

    On lesson 94, 14 minute.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,35 @@
-const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
 // ROUTE HANDLERS
 const getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        console.log('[Query: ]', req.query);
+
+        // Exclude some params from query string
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'field'];
+        excludedFields.forEach((field) => delete queryObj[field]);
+
+        // request queries object directly as a filter after deleting all unnecessary fields
+        const tours = await Tour.find(queryObj);
+
+        // Find by filters
+        // const tours = await Tour.find({
+        //     duration: req.query.duration,
+        //     difficulty: req.query.difficulty
+        // });
+
+        // Find by Mongoose methods
+        // const tours = await Tour.find()
+        //   .where('duration')
+        //   .equals(req.query.duration)
+        //   .where('difficulty')
+        //   .equals(req.query.difficulty);
 
         res.status(200).json({
             status: 'success',
             requestedAt: req.requestTime,
+            results: tours.length,
             data: {
                 tours
             }
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,73 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A Tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
         type: Number,
-        default: 4.5
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
+        // enum: {
+        //     values: ['easy', 'medium', 'difficult'],
+        //     message: 'Difficulty is either: easy, medium, difficult'
+        // }
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        // min: [1, 'Rating must be above 1.0'],
+        // max: [5, 'Rating must be below 5.0']
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    // rating: {
+    //     type: Number,
+    //     default: 4.5
+    // },
+    priceDiscount: {
+        type: Number,
+        // validate: {
+        //     validator: function(val) {
+        //         // this only points to current doc on NEW document creation
+        //         return val < this.price;
+        //     },
+        //     message: 'Discount price ({VALUE}) should be below regular price'
+        // }
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    description: {
+        type: String,
+        trim: true
     },
     price: {
         type: Number,
         required: [true, 'A Tour must have a price']
-    }
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
