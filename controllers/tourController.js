c6503511adac9f793b671b7a040ff050c5226897
Myroslav Pasko <myroslav.pasko@arvosoftware.com>
2021-01-20 06:57:10 +0200

    On lesson 98. Some features with the query. Pagination, advanced filtering, limiting and so on.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,28 +3,76 @@ const Tour = require('./../models/tourModel');
 // ROUTE HANDLERS
 const getAllTours = async (req, res) => {
     try {
-        console.log('[Query: ]', req.query);
 
+        // BUILD QUERY
+        // 1) Filtering
         // Exclude some params from query string
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'limit', 'field'];
         excludedFields.forEach((field) => delete queryObj[field]);
 
+        // 2) Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
         // request queries object directly as a filter after deleting all unnecessary fields
-        const tours = await Tour.find(queryObj);
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 3) Sorting
+        if (req.query.sort) {
+            const sortParams = req.query.sort.split(',').join(' ');
+
+            query = query.sort(sortParams);
+        } else {
+            query = query.sort('-createdAd');
+        }
+
+        // 4) Field limiting
+        if (req.query.fields) {
+            const sortParams = req.query.fields.split(',').join(' ');
+
+            query = query.select(sortParams);
+        } else {
+            query = query.select('-__v');
+        }
+
+        // 5) Pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 3;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+
+            if (skip > numTours) {
+                throw new Error('This page does not exists');
+            }
+        }
 
         // Find by filters
-        // const tours = await Tour.find({
+        // const query = Tour.find({
         //     duration: req.query.duration,
         //     difficulty: req.query.difficulty
         // });
 
         // Find by Mongoose methods
-        // const tours = await Tour.find()
+        // const query = Tour.find()
         //   .where('duration')
         //   .equals(req.query.duration)
         //   .where('difficulty')
         //   .equals(req.query.difficulty);
+
+        //EXECUTE QUERY
+
+        let tours;
+
+        try {
+            tours = await query;
+        } catch (e) {
+
+        }
 
         res.status(200).json({
             status: 'success',
